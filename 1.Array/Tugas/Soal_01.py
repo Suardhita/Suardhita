75ce58f9ac99e74cf34d17aa5f0bf6ec6456fc0b
Suardhita <122293545+Suardhita@users.noreply.github.com>
2026-03-26 00:14:37 +0700

    Add student grades input and processing script

---
+++ 1.Array/Tugas/Soal_01.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+
+def input_nilai(jumlah):
+    data = []
+    print("=== Masukkan Nilai Mahasiswa ===")
+    for i in range(jumlah):
+        while True:
+                n = int(input(f"Masukkan nilai ke-{i+1}: "))
+                if 0 <= n <= 100:
+                    data.append(n)
+                    break
+                else:
+                    print("Nilai harus (0-100)")
+    return data
+
+#  memproses data
+def proses_nilai(data):
+    tertinggi = data[0]
+    terendah = data[0]
+    total = 0
+    lulus = 0
+
+    for n in data:
+        if n > tertinggi:
+            tertinggi = n
+        if n < terendah:
+            terendah = n
+
+        total += n
+
+        if n >= 60:
+            lulus += 1
+
+    rata_rata = total / len(data)
+    tidak_lulus = len(data) - lulus
+
+    return tertinggi, terendah, rata_rata, lulus, tidak_lulus
+
+# menampilkan hasil
+def tampilkan_hasil(data, tertinggi, terendah, rata, lulus , tidak_lulus):
+    print("\n===== Hasil =====")
+    print("Nilai            :", data)
+    print("Nilai tertinggi  :", tertinggi)
+    print("Nilai terendah   :", terendah)
+    print("Rata-rata        :", rata)
+    print("Jumlah lulus     :", lulus)
+    print("Tidak lulus      :", tidak_lulus)
+
+# grafik
+def tampilkan_grafik(tertinggi, terendah, lulus, tidak_lulus):
+    plt.figure()
+    plt.bar(["Tertinggi", "Terendah"], [tertinggi, terendah], color='green')
+    plt.title("Nilai")
+    plt.xlabel("Kategori")
+    plt.ylabel("Nilai")
+    plt.show()
+
+    # grafik lulus
+    plt.figure()
+    plt.bar(["Lulus", "Tidak Lulus"], [lulus, tidak_lulus], color='blue')
+    plt.title("Data Kelulusan")
+    plt.ylabel("Jumlah Mahasiswa")
+    plt.show()
+
+
+# output
+nilai = input_nilai(10)
+tertinggi, terendah, rata, lulus, tidak_lulus = proses_nilai(nilai)
+
+tampilkan_hasil(nilai, tertinggi, terendah, rata, lulus, tidak_lulus)
+tampilkan_grafik(tertinggi, terendah, lulus, tidak_lulus)
